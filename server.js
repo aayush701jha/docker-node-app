49e3a5816f4846145274dbc8fb13b1204b20cf23
Aayush <aayush.jha@kollegeapply.com>
2025-03-13 03:32:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Docker123!");
+  res.send("Hello, Docker from Aayush!");
 });
 
 app.listen(3000, () => {
